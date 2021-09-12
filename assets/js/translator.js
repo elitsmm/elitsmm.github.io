6f873422bb3dcb4dddf99bc87b9793999b099e21
elitsmm <90573329+elitsmm@users.noreply.github.com>
2021-09-12 15:32:42 -0300

    Add files via upload
    Meu Website

---
+++ assets/js/translator.js
@@ -0,0 +1,75 @@
+(function(){
+    const englishInput = document.getElementById('englishInput');
+    const cactobetoInput = document.getElementById('CactobetoInput');
+    const input = document.getElementsByClassName('input');
+    const output = document.getElementsByClassName('output');
+    const translate = document.getElementById('translate');
+    const revert = document.getElementById('revert');
+    const alphabet = {
+        'a': 'y', 'A': 'Y', 
+        'b': 'p', 'B': 'P', 
+        'c': 'l', 'C': 'L', 
+        'd': 't', 'D': 'T', 
+        'e': 'a', 'E': 'A', 
+        'f': 'v', 'F': 'V', 
+        'g': 'k', 'G': 'K', 
+        'h': 'r', 'H': 'R', 
+        'i': 'e', 'I': 'E', 
+        'j': 'z', 'J': 'Z', 
+        'k': 'g', 'K': 'G', 
+        'l': 'm', 'L': 'M', 
+        'm': 's', 'M': 'S', 
+        'n': 'h', 'N': 'H', 
+        'o': 'u', 'O': 'U', 
+        'p': 'b', 'P': 'B', 
+        'q': 'x', 'Q': 'X', 
+        'r': 'n', 'R': 'N', 
+        's': 'c', 'S': 'C', 
+        't': 'd', 'T': 'D', 
+        'u': 'i', 'U': 'I', 
+        'v': 'j', 'V': 'J', 
+        'x': 'q', 'X': 'Q', 
+        'w': 'f', 'W': 'F', 
+        'y': 'o', 'Y': 'O', 
+        'z': 'w', 'Z': 'W', 
+    }
+    const alphabetCactobeto = new Object;
+    for (var letter in alphabet) {
+        alphabetCactobeto[alphabet[letter]] = letter;
+    };
+
+    console.log(alphabetCactobeto);
+
+    revert.addEventListener('click', function(e) {
+        translate.classList.toggle('translateToCactobeto');
+        translate.classList.toggle('translateToEnglish');
+        englishInput.classList.toggle('input');
+        CactobetoInput.classList.toggle('output');
+        englishInput.classList.toggle('output');
+        CactobetoInput.classList.toggle('input');
+        revert.classList.toggle('revert');
+
+        e.preventDefault();
+    });
+
+    translate.addEventListener('click', function(e) {
+        if(translate.className === 'translateToCactobeto') {
+            console.log('>>>');
+            const english = input[0].value;
+            const Cactobeto = english.replaceAll(/([A-Za-z])/g, function (m) {
+                return alphabet[m];
+            });
+            output[0].value = Cactobeto;
+        } 
+        else if(translate.className === 'translateToEnglish') {
+            console.log('<<<');
+            const Cactobeto = input[0].value;
+            const english = Cactobeto.replaceAll(/([A-Za-z])/g, function (m) {
+                return alphabetCactobeto[m];
+            });
+            output[0].value = english;
+        }
+        e.preventDefault();
+    });
+
+})();
