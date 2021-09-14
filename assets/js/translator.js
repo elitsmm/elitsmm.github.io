68dc2bff5c94d1cd1104fb7499ef4bb905f5f510
elitsmm <90573329+elitsmm@users.noreply.github.com>
2021-09-14 14:13:33 -0300

    Update translator.js

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,5 +1,5 @@
 (function(){
-    const portuguêsInput = document.getElementById('portuguêsInput');
+    const englishInput = document.getElementById('englishInput');
     const cactobetoInput = document.getElementById('CactobetoInput');
     const input = document.getElementsByClassName('input');
     const output = document.getElementsByClassName('output');
@@ -42,10 +42,10 @@
 
     revert.addEventListener('click', function(e) {
         translate.classList.toggle('translateToCactobeto');
-        translate.classList.toggle('translateToportuguês');
-        portuguêsInput.classList.toggle('input');
+        translate.classList.toggle('translateToEnglish');
+        englishInput.classList.toggle('input');
         CactobetoInput.classList.toggle('output');
-        portuguêsInput.classList.toggle('output');
+        englishInput.classList.toggle('output');
         CactobetoInput.classList.toggle('input');
         revert.classList.toggle('revert');
 
@@ -55,8 +55,8 @@
     translate.addEventListener('click', function(e) {
         if(translate.className === 'translateToCactobeto') {
             console.log('>>>');
-            const português = input[0].value;
-            const Cactobeto = português.replaceAll(/([A-Za-z])/g, function (m) {
+            const english = input[0].value;
+            const Cactobeto = english.replaceAll(/([A-Za-z])/g, function (m) {
                 return alphabet[m];
             });
             output[0].value = Cactobeto;
@@ -64,10 +64,10 @@
         else if(translate.className === 'translateToEnglish') {
             console.log('<<<');
             const Cactobeto = input[0].value;
-            const português = Cactobeto.replaceAll(/([A-Za-z])/g, function (m) {
+            const english = Cactobeto.replaceAll(/([A-Za-z])/g, function (m) {
                 return alphabetCactobeto[m];
             });
-            output[0].value = português;
+            output[0].value = english;
         }
         e.preventDefault();
     });
