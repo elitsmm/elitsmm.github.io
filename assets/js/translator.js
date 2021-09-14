be4c4c21323782fadcf3d3ecba0641a909c68706
elitsmm <90573329+elitsmm@users.noreply.github.com>
2021-09-14 13:53:40 -0300

    Update translator.js

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,9 +1,9 @@
 (function(){
-    const portuguêsInput = document.getElementById('portuguêsInput');
+    const englishInput = document.getElementById('englishInput');
     const cactobetoInput = document.getElementById('CactobetoInput');
     const input = document.getElementsByClassName('input');
     const output = document.getElementsByClassName('output');
-    const traduzir = document.getElementById('traduzir');
+    const translate = document.getElementById('translate');
     const revert = document.getElementById('revert');
     const alphabet = {
         'a': 'y', 'A': 'Y', 
@@ -41,11 +41,11 @@
     console.log(alphabetCactobeto);
 
     revert.addEventListener('click', function(e) {
-        traduzir.classList.toggle('tranduzirToCactobeto');
-        traduzir.classList.toggle('traduzirToEnglish');
-        portuguêsInput.classList.toggle('input');
+        translate.classList.toggle('translateToCactobeto');
+        translate.classList.toggle('translateToEnglish');
+        englishInput.classList.toggle('input');
         CactobetoInput.classList.toggle('output');
-        portuguêsInput.classList.toggle('output');
+        englishInput.classList.toggle('output');
         CactobetoInput.classList.toggle('input');
         revert.classList.toggle('revert');
 
@@ -53,21 +53,21 @@
     });
 
     translate.addEventListener('click', function(e) {
-        if(translate.className === 'traduzirToCactobeto') {
+        if(translate.className === 'translateToCactobeto') {
             console.log('>>>');
-            const português = input[0].value;
-            const Cactobeto = português.replaceAll(/([A-Za-z])/g, function (m) {
+            const english = input[0].value;
+            const Cactobeto = english.replaceAll(/([A-Za-z])/g, function (m) {
                 return alphabet[m];
             });
             output[0].value = Cactobeto;
         } 
-        else if(traduzir.className === 'tranduzirToEnglish') {
+        else if(translate.className === 'translateToEnglish') {
             console.log('<<<');
             const Cactobeto = input[0].value;
-            const português = Cactobeto.replaceAll(/([A-Za-z])/g, function (m) {
+            const english = Cactobeto.replaceAll(/([A-Za-z])/g, function (m) {
                 return alphabetCactobeto[m];
             });
-            output[0].value = português;
+            output[0].value = english;
         }
         e.preventDefault();
     });
